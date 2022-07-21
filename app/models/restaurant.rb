7914bc7d240a8f4adc8aa2c6a29cad8643cf3fad
Elizabeth Chapman <elizabethmaychapman@gmail.com>
2022-07-21 21:58:40 +0100

    Yelp MVP progress save

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
-
-  validates :title, :address, :category, presence: true
+    CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+    validates :name, :address, presence: :true
+    validates :category, inclusion: { in: CATEGORIES }
+    has_many :reviews, dependent: :destroy
 end
